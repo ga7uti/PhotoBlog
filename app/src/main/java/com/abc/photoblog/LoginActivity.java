1d38514dd21a8d63fa5cbabaacc5ab9a2dd30820
Gautam <gauti783@gmail.com>
2018-07-01 20:16:56 +0530

    Added Login Activity

--- app/src/main/java/com/abc/photoblog/LoginActivity.java
+++ app/src/main/java/com/abc/photoblog/LoginActivity.java
@@ -1,15 +1,13 @@
 package com.abc.photoblog;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        setContentView(R.layout.activity_login);
     }
 }
