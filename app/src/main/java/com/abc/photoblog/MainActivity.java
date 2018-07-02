72ec88bbefb1e0823f97faa9965cc01fc8ed0d5a
Gautam <gauti783@gmail.com>
2018-07-02 10:52:30 +0530

    Added Logout option

--- app/src/main/java/com/abc/photoblog/MainActivity.java
+++ app/src/main/java/com/abc/photoblog/MainActivity.java
@@ -3,25 +3,66 @@ package com.abc.photoblog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null){
-            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            switchActivity();
             finish();
         }
         super.onStart();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_logout_btn:
+                logout();
+                return true;
+
+
+                default:
+                return false;
+        }
+    }
+
+    private void logout() {
+        firebaseAuth.signOut();
+        switchActivity();
+    }
+
+    private void switchActivity() {
+        Intent intent=new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
